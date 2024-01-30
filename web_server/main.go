eae8b69691337bff5198f3d1d1dacbee9fcb81de
Lam LUL <nguyenduclam1711@gmail.com>
2024-01-30 16:39:14 +0700

    Add compress midleware and config static files

--- web_server/main.go
+++ web_server/main.go
@@ -4,14 +4,16 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/hello", func(c *fiber.Ctx) error {
-		return c.SendString("hohohohohoho")
-	})
+	// use compress middleware
+	app.Use(compress.New())
+
+	app.Static("/", "../public")
 
 	log.Fatal(app.Listen(":3000"))
 }
